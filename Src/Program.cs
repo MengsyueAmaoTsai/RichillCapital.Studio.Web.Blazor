ca7a9d0316554d5ef2bb60a8fe22bf42a8e46221
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2023-11-20 02:15:16 +0800

    refactor with WebApplicationExtensions

--- Src/Program.cs
+++ Src/Program.cs
@@ -1,33 +1,11 @@
-using RichillCapital.Studio.Web.Components;
+using RichillCapital.Studio.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddInteractiveWebAssemblyComponents();
+var app = builder
+    .ConfigureServices()
+    .Build();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
-else
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode()
-    .AddInteractiveWebAssemblyRenderMode();
-
-await app.RunAsync();
+await app
+    .ConfigurePipeline()
+    .RunAsync();
---
+++ Src/WebApplicationExtensions.cs
@@ -0,0 +1,43 @@
+using RichillCapital.Studio.Web.Components;
+
+namespace RichillCapital.Studio.Web;
+
+public static class WebApplicationExtensions
+{
+    public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services
+            .AddRazorComponents()
+            .AddInteractiveServerComponents()
+            .AddInteractiveWebAssemblyComponents();
+
+        return builder;
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode()
+            .AddInteractiveWebAssemblyRenderMode();
+
+        return app;
+    }
+}
